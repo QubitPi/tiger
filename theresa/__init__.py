f797787590f2bb1198be271969f1e59bf2fc5435
活泼到令人头疼的胡桃 <jack20220723@gmail.com>
2023-07-05 15:57:46 +0800

    Implement expand endpoint (#17)
    * Implement Google Knowledge Graph based node expand
    
    * First draft is done
    
    * Enhance Swagger doc
    
    * Enhance Swagger doc

--- theresa/__init__.py
+++ theresa/__init__.py
@@ -1,8 +1,10 @@
 from flask_cors import CORS
+import json
 from flasgger import Swagger
 from flask import Flask, request, jsonify
 from theresa.entity_extraction.rapid_api import entity_extraction
 from theresa.entity_extraction.rapid_api import transform_to_knowledge_graph_spec
+from theresa.expand.google_knowledge_graph_api import node_expand
 
 
 def create_app():
@@ -19,10 +21,11 @@ def create_app():
     @app.route("/entityExtraction")
     def named_entity_extraction():
         """
-        This endpoint returns the Named Entity Recognition result of a sentence
+        对一段文字进行知识抽取分析
         ---
         parameters:
           - name: sentence
+            description: 被分析的文字，可以是一个句子，也可以是一段话，内容格式和长度不限
             in: query
             type: string
             required: true
@@ -32,5 +35,45 @@ def create_app():
         """
         rapid_api_result = entity_extraction([request.args.get('sentence')])
         return jsonify(transform_to_knowledge_graph_spec(rapid_api_result))
+
+    @app.route("/expand")
+    def expand():
+        """
+        对一个节点进行展开操作并返回与之相关的新节点和关系
+        ---
+        parameters:
+          - name: node
+            description: |
+              一个节点对象，举例如下
+              对象必须包含 "id" 和 "fields" 两个属性
+              "id" 是这个节点的在一张图谱中的唯一标识
+              "fields" 是这个节点上的属性（即前端 NodeModel 中 "propertiesList" 里的 key-value pairs），"fields" 可以包含任何属性
+            in: query
+            required: true
+            type: object
+            schema:
+              type: object
+              properties:
+                id:
+                  type: string
+                  required: true
+                fields:
+                  type: object
+                  required: true
+              example:
+                id: "TypeScript"
+                fields:
+                  name: "TypeScript"
+                  anyOtherFields1: "foo"
+                  anyOtherFields2: "bar"
+        responses:
+          200:
+            description: 一张独立的知识图谱
+          400 ~ 499:
+            description: 前端请求参数不合法，请确保 "node" 传参和上面的举例一致
+          500+:
+            description: 后端服务异常，请通知 Jack 查看
+        """
+        return jsonify(node_expand(json.loads(request.args.get('node'))))
 
     return app
---
+++ theresa/expand/google_knowledge_graph_api.py
@@ -0,0 +1,90 @@
+import requests
+from requests import Response
+from flask import current_app # https://stackoverflow.com/a/32017603
+
+from theresa.entity_extraction.rapid_api import entity_extraction
+from theresa.entity_extraction.rapid_api import transform_to_knowledge_graph_spec
+
+
+def get_queries(node) -> list[str]:
+    """
+    Tokenizing a node fields.
+
+    Each field will fire a Google Knowledge Graph API
+
+    :param node:  An object representing a Knowledge Graph Spec node
+
+    :return: a list of Google Knowledge Graph API search queries
+    """
+    queries = []
+    for field_name, field_value in node["fields"].items():
+        queries.append(field_value.replace(" ", "+"))
+    return queries
+
+
+def node_expand(node: object):
+    """
+    Expands a given node.
+
+    :param node:  An object representing a Knowledge Graph Spec node
+
+    :return: a Knowledge Graph Spec graph what guarantees to contain the node being expanded
+    """
+
+    queries = get_queries(node)
+
+    nodes = []
+    for query in queries:
+        response_body = fire_request(query).json()
+
+        if response_body["itemListElement"]:
+            if response_body["itemListElement"][0]:
+                if response_body["itemListElement"][0]["result"]:
+                    if response_body["itemListElement"][0]["result"]["detailedDescription"]:
+                        if response_body["itemListElement"][0]["result"]["detailedDescription"]["articleBody"]:
+                            nodes.extend(
+                                transform_to_knowledge_graph_spec(
+                                    entity_extraction(
+                                        [
+                                            response_body["itemListElement"][0]["result"]["detailedDescription"][
+                                                "articleBody"]
+                                        ]
+                                    )
+                                )["nodes"]
+                            )
+
+    for expanded_node in nodes:
+        if node["id"] == expanded_node["id"]:
+            nodes.remove(expanded_node)
+
+    links = []
+    for expanded_node in nodes:
+        links.append({
+            "source": node["id"],
+            "target": expanded_node["id"]
+        })
+
+    nodes.append(node)
+
+    return {
+        "nodes": nodes,
+        "links": links
+    }
+
+
+def fire_request(query: str) -> Response:
+    """
+    Sends expand request to Google Knowledge Graph API.
+
+    :param query:  The search term
+
+    :return: a Google Knowledge Graph API response object
+    """
+    QUERY_TEMPLATE = "https://kgsearch.googleapis.com/v1/entities:search?query={query}&key={api_key}&limit=1&indent=True"
+    url = QUERY_TEMPLATE.format(query=query, api_key=current_app.config['GOOGLE_KNOWLEDGE_GRAPH_API_KEY'])
+
+    return requests.get(url)
+
+
+if __name__ == '__main__':
+    pass
