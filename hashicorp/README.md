05e5d9d1ab1c5ddc70dccc7926646a728060f601
活泼到令人头疼的胡桃 <jack20220723@gmail.com>
2023-07-17 14:31:15 +0800

    Detect language during node expand (#30)
    * Detect language during node expand
    
    * Self-review
    
    * Self-review
    
    * Self-review

--- hashicorp/README.md
+++ hashicorp/README.md
@@ -7,3 +7,24 @@ Theresa has been deployed to
 - **machine-learning.paion-data.com** (single EC2 instance with dedicated SSL certificate): serving
   [Theresa Rapid API](https://rapidapi.com/paion-data-machine-learning/api/theresa3)
 - **3 EC2 instances without certificates behind load balancer**: serving app.nexusgraph.com production environment
+
+The following GitHub Secrets needs to be defined:
+
+- GH_PAT_READ
+
+- AWS
+- AWS_ACCESS_KEY_ID
+- AWS_REGION
+- AWS_SECRET_ACCESS_KEY
+
+- SSL_CERTIFICATE_DEV
+- SSL_CERTIFICATE_KEY_DEV
+- SSL_CERTIFICATE_KEY_PROD
+- SSL_CERTIFICATE_PROD
+
+- OPEN_AI_API_KEY
+
+- GOOGLE_KNOWLEDGE_GRAPH_API_KEY
+
+- X_RAPIDAPI_KEY_MICROSOFT_ENTITY_EXTRACTION
+- X_RAPIDAPI_KEY_MICROSOFT_LANGUAGE_DETECTION
--- theresa/expand/google_knowledge_graph_api.py
+++ theresa/expand/google_knowledge_graph_api.py
@@ -1,11 +1,63 @@
 import requests
 from requests import Response
-from flask import current_app # https://stackoverflow.com/a/32017603
+from flask import current_app  # https://stackoverflow.com/a/32017603
 
 from theresa.entity_extraction.graph_gpt import entity_extraction
 
 
-def get_queries(node) -> list[str]:
+def _detect_language_as_either_zh_or_en(text: str):
+    """
+    Returns the most probable language (either Chinese or English) of a specified text via RapidAPI at
+    https://rapidapi.com/microsoft-azure-org-microsoft-cognitive-services/api/microsoft-text-analytics1/ (Detect
+    Language)
+
+    :param text:  The provided text to be analyzed
+
+    :return: one of two values: "zh" or "en"
+    """
+    url = "https://microsoft-text-analytics1.p.rapidapi.com/languages"
+    headers = {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": current_app.config['X_RAPIDAPI_KEY_MICROSOFT_LANGUAGE_DETECTION'],
+        "X-RapidAPI-Host": "microsoft-text-analytics1.p.rapidapi.com"
+    }
+    payload = {
+        "documents": [{
+            "id": "1",
+            "text": text
+        }]
+    }
+    iso6391Name: str = requests.post(url, json=payload, headers=headers).json()["documents"][0]["detectedLanguage"][
+        "iso6391Name"]
+
+    if iso6391Name == "zh_chs":
+        return "zh"
+
+    return "en"
+
+
+def _fire_request(query: str) -> Response:
+    """
+    Sends expand request to Google Knowledge Graph API.
+
+    :param query:  The search term
+
+    :return: a Google Knowledge Graph API response object (Not in Knowledge Graph Spec format)
+    """
+    iso_language: str = _detect_language_as_either_zh_or_en(query)
+
+    QUERY_TEMPLATE = "https://kgsearch.googleapis.com/v1/entities:search?query={query}&key={" \
+                     "api_key}&limit=1&indent=True&languages={language}"
+    url = QUERY_TEMPLATE.format(
+        query=query,
+        api_key=current_app.config['GOOGLE_KNOWLEDGE_GRAPH_API_KEY'],
+        language=iso_language
+    )
+
+    return requests.get(url)
+
+
+def _get_queries(node) -> list[str]:
     """
     Tokenizing a node fields.
 
@@ -30,12 +82,12 @@ def node_expand(node: object):
     :return: a Knowledge Graph Spec graph what guarantees to contain the node being expanded
     """
 
-    queries = get_queries(node)
+    queries = _get_queries(node)
 
     nodes = []
     links = []
     for query in queries:
-        response_body = fire_request(query).json()
+        response_body = _fire_request(query).json()
 
         if response_body["itemListElement"]:
             if response_body["itemListElement"][0]:
@@ -43,11 +95,11 @@ def node_expand(node: object):
                     if response_body["itemListElement"][0]["result"]["detailedDescription"]:
                         if response_body["itemListElement"][0]["result"]["detailedDescription"]["articleBody"]:
                             expanded_graph = entity_extraction(
-                                        [
-                                            response_body["itemListElement"][0]["result"]["detailedDescription"][
-                                                "articleBody"]
-                                        ]
-                                    )
+                                [
+                                    response_body["itemListElement"][0]["result"]["detailedDescription"][
+                                        "articleBody"]
+                                ]
+                            )
 
                             nodes.extend(expanded_graph["nodes"])
                             links.extend(expanded_graph["links"])
@@ -68,20 +120,6 @@ def node_expand(node: object):
         "nodes": nodes,
         "links": links
     }
-
-
-def fire_request(query: str) -> Response:
-    """
-    Sends expand request to Google Knowledge Graph API.
-
-    :param query:  The search term
-
-    :return: a Google Knowledge Graph API response object
-    """
-    QUERY_TEMPLATE = "https://kgsearch.googleapis.com/v1/entities:search?query={query}&key={api_key}&limit=1&indent=True"
-    url = QUERY_TEMPLATE.format(query=query, api_key=current_app.config['GOOGLE_KNOWLEDGE_GRAPH_API_KEY'])
-
-    return requests.get(url)
 
 
 if __name__ == '__main__':
