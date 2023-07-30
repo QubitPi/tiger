81958c0c200c2da13cf7603b6a803d6790d5f7e9
Jack <jack20220723@gmail.com>
2023-07-30 11:03:12 +0800

    Decommissioning machine-learning.paion-data.dev

--- hashicorp/instances/public.tf
+++ hashicorp/instances/public.tf
@@ -1,18 +1,3 @@
-data "aws_ami" "latest-theresa-dev" {
-  most_recent = true
-  owners = ["899075777617"]
-
-  filter {
-    name   = "name"
-    values = ["theresa-dev"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
 data "aws_ami" "latest-theresa-prod" {
   most_recent = true
   owners = ["899075777617"]
@@ -26,42 +11,6 @@ data "aws_ami" "latest-theresa-prod" {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-}
-
-resource "aws_instance" "theresa-dev" {
-  ami = "${data.aws_ami.latest-theresa-dev.id}"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "Paion Data Theresa (Dev)"
-  }
-  security_groups = ["Paion Data Theresa"]
-
-  user_data = <<-EOF
-    #!/bin/bash
-    cd /home/ubuntu/theresa
-
-    alias python=python3.10
-    alias python3=python3.10
-
-    python3.10 -m venv .venv
-    . .venv/bin/activate
-    python3.10 -m pip install .
-    export APP_CONFIG_FILE=/home/ubuntu/settings.cfg
-
-    sudo nginx -t
-    sudo nginx -s reload
-
-    gunicorn -w 4 -b 0.0.0.0 'theresa:create_app()'
-  EOF
-}
-
-resource "aws_route53_record" "machine-learning-paion-data-dev" {
-  zone_id         = "Z02600613NNEBWDLMOCCJ"
-  name            = "machine-learning.paion-data.dev"
-  type            = "A"
-  ttl             = 300
-  records         = [aws_instance.theresa-dev.public_ip]
-  allow_overwrite = true
 }
 
 resource "aws_instance" "theresa-prod" {
