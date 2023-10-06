49f4515963751904caf0a8370ebb98fac44ed951
Jack <jack20220723@gmail.com>
2023-10-06 21:20:16 +0800

    Reduce instance to 1 for nexusgraph

--- hashicorp/instances/nexusgraph.tf
+++ hashicorp/instances/nexusgraph.tf
@@ -17,55 +17,7 @@ resource "aws_instance" "paion-data-nexusgraph-theresa-1" {
   ami = "${data.aws_ami.latest-nexusgraph-theresa.id}"
   instance_type = "t2.small"
   tags = {
-    Name = "Paion Data nexusgraph Theresa 1"
-  }
-  security_groups = ["Paion Data nexusgraph Theresa"]
-
-  user_data = <<-EOF
-    #!/bin/bash
-    cd /home/ubuntu/theresa
-
-    alias python=python3.10
-    alias python3=python3.10
-
-    python3.10 -m venv .venv
-    . .venv/bin/activate
-    python3.10 -m pip install .
-    export APP_CONFIG_FILE=/home/ubuntu/settings.cfg
-
-    gunicorn -w 4 -b 0.0.0.0 'theresa:create_app()'
-  EOF
-}
-
-resource "aws_instance" "paion-data-nexusgraph-theresa-2" {
-  ami = "${data.aws_ami.latest-nexusgraph-theresa.id}"
-  instance_type = "t2.small"
-  tags = {
-    Name = "Paion Data nexusgraph Theresa 2"
-  }
-  security_groups = ["Paion Data nexusgraph Theresa"]
-
-  user_data = <<-EOF
-    #!/bin/bash
-    cd /home/ubuntu/theresa
-
-    alias python=python3.10
-    alias python3=python3.10
-
-    python3.10 -m venv .venv
-    . .venv/bin/activate
-    python3.10 -m pip install .
-    export APP_CONFIG_FILE=/home/ubuntu/settings.cfg
-
-    gunicorn -w 4 -b 0.0.0.0 'theresa:create_app()'
-  EOF
-}
-
-resource "aws_instance" "paion-data-nexusgraph-theresa-3" {
-  ami = "${data.aws_ami.latest-nexusgraph-theresa.id}"
-  instance_type = "t2.small"
-  tags = {
-    Name = "Paion Data nexusgraph Theresa 3"
+    Name = "Paion Data Nexus Graph Theresa 1"
   }
   security_groups = ["Paion Data nexusgraph Theresa"]
 
